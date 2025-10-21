e3591ee568acea2657d777a359b855af15b41493
Jeremy Lin <jeremy60572@gmail.com>
2025-10-21 01:43:11 -0700

    Implemented Gemini prompt and response

--- src/gemini-start.js
+++ src/gemini-start.js
@@ -1,18 +1,18 @@
-import dotenv from 'dotenv';
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
+const apiKey = process.env.API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 async function run() {
-    const model = genAI.getGenerativeModel('gemini-pro');
-
-    const prompt = "what does meat taste like?";
-
-    const result = await model.generateText(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text)
+  const genai=new GoogleGenerativeAI(process.env.API_KEY)
+  const model=genai.getGenerativeModel({model:"gemini-2.5-flash"})
+  const prompt = "what does meat taste like?";
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
 }
 
 run();
