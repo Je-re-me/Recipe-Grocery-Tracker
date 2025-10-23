064d3f22733505d3d5ed955d0566c21f3447cc26
Jeremy Lin <jeremy60572@gmail.com>
2025-10-23 00:13:33 -0700

    Created component and container folders

--- src/components/Gemini.js
+++ src/components/Gemini.js
@@ -3,11 +3,11 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
 const apiKey = process.env.API_KEY;
-const genAI = new GoogleGenerativeAI(apiKey);
 
-async function run() {
+async function run(fuckingUserInput) {
   const genai=new GoogleGenerativeAI(process.env.API_KEY)
   const model=genai.getGenerativeModel({model:"gemini-2.5-flash"})
+  
   const prompt = "what does meat taste like?";
   const result = await model.generateContent(prompt);
   const response = await result.response;
